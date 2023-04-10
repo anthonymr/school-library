0c24ba246461ca5714f00e87247ce7e664c94a45
Anthony Martin <anthonymartin20@gmail.com>
2023-04-10 12:14:14 -0400

    Correct linter offenses

--- decorators/trimmer_decorator.rb
+++ decorators/trimmer_decorator.rb
@@ -2,7 +2,8 @@ require_relative 'decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    return  @nameable.correct_name unless @nameable.correct_name.length > 10
+    return @nameable.correct_name unless @nameable.correct_name.length > 10
+
     super[0..9]
   end
 end
