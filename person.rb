20a8b65710dc4c616d451bc66ec6ca28508fe16f
Anthony Martin <anthonymartin20@gmail.com>
2023-04-10 10:53:43 -0400

    Make Person class nameable

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
@@ -15,6 +17,10 @@ class Person
 
   def of_age?
     @age >= 18
+  end
+
+  def correct_name
+    @name
   end
 
   private :is_of_age?
