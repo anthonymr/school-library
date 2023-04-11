98bf6a95f031976f6ed6369247cc94a4d07a5f0a
Anthony Martin <anthonymartin20@gmail.com>
2023-04-11 10:36:20 -0400

    Runing some tests

--- main.rb
+++ main.rb
@@ -1,10 +1,52 @@
 require_relative 'person'
-require_relative 'decorators/trimmer_decorator'
-require_relative 'decorators/capitalize_decorator'
+require_relative 'book'
+require_relative 'rental'
+require_relative 'classroom'
+require_relative 'student'
+require_relative 'teacher'
 
-person = Person.new(22, name: 'maximilianus')
-p person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-p capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-p capitalized_trimmed_person.correct_name
+math_classroom = Classroom.new('Mathematics')
+
+person = Person.new(18, name: 'person1')
+student1 = Student.new(30, math_classroom, name: 'student1')
+student2 = Student.new(45, math_classroom, name: 'student2')
+teacher = Teacher.new(55, nil, name: 'teacher1')
+
+book1 = Book.new('Book1 title', 'Book1 author')
+book2 = Book.new('Book2 title', 'Book2 author')
+
+# person has rented book1 and book2
+rental = Rental.new('2021-01-01', book1, person)
+rental = Rental.new('2021-01-01', book2, person)
+# student1 has rented book1
+rental = Rental.new('2021-01-01', book1, student1)
+# techer1 has rented book2
+rental = Rental.new('2021-01-01', book2, teacher)
+
+# Adding studets to math_classroom
+math_classroom.add_student(student1)
+math_classroom.add_student(student2)
+
+print "Who has rented book1?: "
+puts book1.rentals.map {|rental| rental.person.name}.join(', ')
+
+print "Who has rented book2?: "
+puts book2.rentals.map {|rental| rental.person.name}.join(', ')
+
+print "Person rentals: "
+puts person.rentals.map {|rental| rental.book.title}.join(', ')
+
+print "Student1 rentals: "
+puts student1.rentals.map {|rental| rental.book.title}.join(', ')
+
+print "Teacher1 rentals: "
+puts teacher.rentals.map {|rental| rental.book.title}.join(', ')
+
+print "Students in math_classroom: "
+puts math_classroom.students.map {|student| student.name}.join(', ')
+
+print "Student1 classroom: "
+puts student1.classroom.label
+
+print "Student2 classroom: "
+puts student2.classroom.label
